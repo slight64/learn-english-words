ebd71e6f3e586b392ca93ca4f3c8ae46a69472d9
Vladimir <slight1989@gmail.com>
2024-01-12 01:24:26 +0400

    feat: add redux-persist

--- src/features/redux/index.ts
+++ src/features/redux/index.ts
@@ -1,13 +1,40 @@
 import quizReducer from './../dictionary/quizSlice';
 import dictionaryReducer from './../dictionary/dictionarySlice';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-export const store = configureStore({
-  reducer: {
-    dictionary: dictionaryReducer,
-    quiz: quizReducer,
-  },
+const rootReducer = combineReducers({
+  dictionary: dictionaryReducer,
+  quiz: quizReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
