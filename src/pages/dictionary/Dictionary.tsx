54a192ab6ba5d5d5c396bd801a1d1e047423e0b3
Vladimir <slight1989@gmail.com>
2024-01-12 01:33:25 +0400

    refactoring: removed old localstorage functionality

--- src/pages/dictionary/Dictionary.tsx
+++ src/pages/dictionary/Dictionary.tsx
@@ -1,18 +1,11 @@
-import { useEffect } from 'react';
 import Input from '../../entities/ui/Input/Input';
-
 import Loader from '../../entities/ui/Loader/Loader';
 import WordCard from '../../entities/ui/WordCard/WordCard';
-import { useAppDispatch, useAppSelector } from '../../features/redux/hooks';
+import { useAppSelector } from '../../features/redux/hooks';
 import './dictionary.scss';
-import { createDictionaryFromStorage } from '../../features/dictionary/dictionarySlice';
 
 const Dictionary = () => {
   const data = useAppSelector((state) => state.dictionary.words);
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(createDictionaryFromStorage());
-  }, []);
 
   return (
     <div className="dictionary">
